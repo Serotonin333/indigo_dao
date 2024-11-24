85f459bd3d928e87a5f8ed666b156ef79fc391f9
Serotonin333 <tanman379_@hotmail.com>
2024-11-23 22:42:59 -0600

    auto formatted with black

--- buyback_rec/database_builder.py
+++ buyback_rec/database_builder.py
@@ -10,13 +10,13 @@ from time import sleep
 from pathlib import Path
 
 conn = http.client.HTTPSConnection("api.exchange.coinbase.com")
-res_agg_lim = 300   # most aggregations coinbase will return at once
-req_per_min_lim = 10    # says 10 per second, but that doesn't seem right for public API
+res_agg_lim = 300  # most aggregations coinbase will return at once
+req_per_min_lim = 10  # says 10 per second, but that doesn't seem right for public API
 sleep_time = 60 / req_per_min_lim
 payload = ""
 headers = {"Content-Type": "application/json", "User-Agent": "someone"}
 token = "BTC"
-token2 = 'USD'
+token2 = "USD"
 
 # -------Market Depth------
 # level=1 gives the highest bid and lowest ask
@@ -30,22 +30,31 @@ token2 = 'USD'
 # data = res.read()
 
 # -----Candles------
-granularity=86400   # 1 day = 86400 s
-start_date = datetime.datetime(2017,1,1)
+granularity = 86400  # 1 day = 86400 s
+start_date = datetime.datetime(2017, 1, 1)
 end_date = datetime.datetime.now()
 start = start_date
-stop = min(start + datetime.timedelta(seconds=(res_agg_lim-1)*granularity), end_date)
+stop = min(
+    start + datetime.timedelta(seconds=(res_agg_lim - 1) * granularity), end_date
+)
 running_table = SCHEMA_CANDLE.empty_table()
 table_size_lim = 100 * 1024**2  # X MB in bytes
 database = Path.cwd() / "buyback_rec/database"
 while start < end_date:
-    conn.request("GET", f"/products/{token}-{token2}/candles?start={start.isoformat()}&end={stop.isoformat()}&granularity={granularity}", payload, headers)
+    conn.request(
+        "GET",
+        f"/products/{token}-{token2}/candles?start={start.isoformat()}&end={stop.isoformat()}&granularity={granularity}",
+        payload,
+        headers,
+    )
     res = conn.getresponse()
     res_data = res.read()
     json_obj = json.loads(res_data.decode("utf-8"))
 
-    data = pd.DataFrame(json_obj, columns=['start_time','low', 'high', "open", 'close', 'volume' ])
-    data["start_time"] = pd.to_datetime(data["start_time"], unit='s')
+    data = pd.DataFrame(
+        json_obj, columns=["start_time", "low", "high", "open", "close", "volume"]
+    )
+    data["start_time"] = pd.to_datetime(data["start_time"], unit="s")
     data["asset1"] = token
     data["asset2"] = token2
 
@@ -53,9 +62,12 @@ while start < end_date:
     running_table = pa.concat_tables([running_table, table])
 
     start = stop + datetime.timedelta(seconds=granularity)
-    stop = min(start + datetime.timedelta(seconds=(res_agg_lim-1)*granularity), end_date)
-    sleep(sleep_time)   # sleep to prevent over requesting from API
+    stop = min(
+        start + datetime.timedelta(seconds=(res_agg_lim - 1) * granularity), end_date
+    )
+    sleep(sleep_time)  # sleep to prevent over requesting from API
 
 running_table = running_table.combine_chunks()
-pq.write_table(running_table, (database / f"{token}-{token2}_candles.parquet").absolute())
-
+pq.write_table(
+    running_table, (database / f"{token}-{token2}_candles.parquet").absolute()
+)
