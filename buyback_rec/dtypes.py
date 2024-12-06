072fd6146f138f4a4923632e8ce699c742ef924b
Serotonin333 <tanman379_@hotmail.com>
2024-12-05 21:12:17 -0600

    added start_price to settings, removed unused np dtypes

--- buyback_rec/dtypes.py
+++ buyback_rec/dtypes.py
@@ -1,16 +1,4 @@
-import numpy as np
 import pyarrow as pa
-
-candle_np = [
-    ("asset1", np.str_),
-    ("asset2", np.str_),
-    ("start_time", "datetime64[s]"),
-    ("low", np.float32),
-    ("high", np.float32),
-    ("open", np.float32),
-    ("close", np.float32),
-    ("volume", np.float32),
-]
 
 candle_pa = [
     ("asset1", pa.string()),
@@ -23,36 +11,6 @@ candle_pa = [
     ("volume", pa.float32()),
 ]
 
-buyback_np = [
-    ("identifier", np.str_),
-    ("start_time", "datetime64[s]"),
-    ("last_reset_time", "datetime64[s]"),
-    ("trigger_time", "datetime64[s]"),
-    ("amount", np.float32),
-    ("price", np.float32),
-    ("purchased", np.float32),
-    ("ref_price", np.float32),
-    ("ratio", np.float32),
-    ("discount", np.float32),
-    ("start_price", np.float32),
-    ("running_allocated", np.float32),
-    ("running_spent", np.float32),
-    ("running_purchased", np.float32),
-    ("running_return", np.float32),
-    ("remaining_amount", np.float32),
-]
-
-overview_np = [
-    ("identifier", np.str_),
-    ("ratio", np.float32),
-    ("discount", np.float32),
-    ("delay_min", "timedelta64[s]"),
-    ("delay_max", "timedelta64[s]"),
-    ("delay_mean", "timedelta64[s]"),
-    ("end_running_return", np.float32),
-    ("end_discount_running_return", np.float32),
-    ("running_return_mean", np.float32),
-]
 
 buyback_pa = [
     ("identifier", pa.string()),
@@ -104,11 +62,9 @@ settings_pa = [
     ("asset1", pa.string()),
     ("asset2", pa.string()),
     ("redistribute_on_refresh", pa.bool_()),
+    ("start_price", pa.float32()),
 ]
 
-DTYPE_CANDLE = np.dtype(candle_np)
-DTYPE_BUYBACK = np.dtype(buyback_np)
-DTYPE_OVERVIEW = np.dtype(overview_np)
 
 SCHEMA_CANDLE = pa.schema(candle_pa)
 SCHEMA_BUYBACK = pa.schema(buyback_pa)
@@ -116,6 +72,5 @@ SCHEMA_OVERVIEW = pa.schema(overview_pa)
 SCHEMA_SETTINGS = pa.schema(settings_pa)
 
 if __name__ == "__main__":
-    print(DTYPE_BUYBACK)
     print(SCHEMA_BUYBACK)
     print(SCHEMA_SETTINGS)
